5e4e1da3aa661e947c131bfbe175a164f7d831db
Sirajul <shirajulislamparvez@gmail.com>
2023-03-05 20:56:41 +0600

    Add Route

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
   }
 }
 class HomeActivity extends StatelessWidget{
-  HomeActivity({super.key});
+   HomeActivity({super.key});
 
   @override
   Widget build(BuildContext context){
@@ -26,44 +26,57 @@ class HomeActivity extends StatelessWidget{
       body:Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityOne()));}, child: Text("Go Activity 01")),
-          ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityTwo()));}, child: Text("Go Activity 02"))
+          ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityOne("This is From Home to 1")));}, child: Text("Go Activity 01")),
+          ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityTwo("This is From Home to 2")));}, child: Text("Go Activity 02"))
         ],
       )
     );
   }
 }
 
+//Activity One
 class PageActivityOne extends StatelessWidget{
-  PageActivityOne({super.key});
+
+  String Messege;
+   PageActivityOne(
+      this.Messege,
+      {super.key}
+      );
+
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Page 1 Activity"),),
+      appBar: AppBar(title: Text(Messege),),
         body:Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));}, child: Text("Go On Home")),
-            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityTwo()));}, child: Text("Go Activity 02"))
+            //ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));}, child: Text("Go On Home")),
+            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityTwo("This is From 1 to 2")));}, child: Text("Go Activity 02"))
           ],
         )
     );
   }
 }
+
+//Activity Two
 class PageActivityTwo extends StatelessWidget{
-  PageActivityTwo({super.key});
+
+  String Messege;
+   PageActivityTwo(
+      this.Messege,{super.key});
+
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Page 2 Activity"),),
+      appBar: AppBar(title: Text(Messege),),
         body:Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));}, child: Text("Go On Home")),
+            //ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));}, child: Text("Go On Home")),
 
-            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityOne()));}, child: Text("Go Activity 01"))
+            ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>PageActivityOne("This is From 2 to 1")));}, child: Text("Go Activity 01"))
           ],
         )
     );
